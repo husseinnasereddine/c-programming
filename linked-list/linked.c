5cba3d1d4a7cd5f97085d2e0e1189ee46af97fd9
root <root@localhost.localdomain>
2017-02-16 11:51:22 -0600

    final version of structure

--- linked-list/linked.c
+++ linked-list/linked.c
@@ -1,26 +1,15 @@
 #include<stdio.h>
 
-
-
-
 struct node {
-
 int data;
-
 };
-
 int add ()
 {
-
 int a;
 printf ("pelase enter a number\n");
 scanf("%d",&a);
-
 return a;
 }
-
-
-
 void display (struct node node1[10], int pos)
 {
   for(int i=0 ;i<pos; i++)
@@ -28,17 +17,11 @@ void display (struct node node1[10], int pos)
     printf("\nposition number %d: %d\n",i,node1[i].data);
   }
 }
-
-
-
 int main (int argv, char *agrc[])
 {
-
 struct node node1[10];
 int i=0;
 int option =0;
-
-
 do{
   printf("\nplease choose option 1 | 2 | 3\n");
   scanf("%d",&option);
@@ -61,15 +44,6 @@ switch(option)
   printf("\nchoose the right option\n");
   break;
 }
-
-
 }while(option != 3);
-
-
-
-
-
-
-
 return 0;
 }
